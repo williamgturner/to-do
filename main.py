8d1621ef80b86eddc68457ba7d4628c1b76b80c1
Will Turner <willguyturner@gmail.com>
2025-02-15 18:07:11 +1300

    Add CSS styling

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header
+from textual.widgets import Button, Digits, Footer, Header, Label
+from textual.containers import HorizontalGroup, VerticalGroup, VerticalScroll
 
+class DayFrame(VerticalGroup):
+    """A singular day in the calendar"""
+    def compose(self) -> ComposeResult:
+        yield Label("Day")
+        yield Label("Event")
+        yield Button("Delete")
 class TodoApp(App):
     """A textual organistion app."""
 
+    CSS_PATH = "./to-do.tcss"
     BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
-
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
         yield Header()
         yield Footer()
+        yield VerticalScroll(DayFrame(), DayFrame(), DayFrame())
 
     def action_toggle_dark(self) -> None:
         """An action to toggle dark mode."""
