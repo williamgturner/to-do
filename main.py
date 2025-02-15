dd7d1cac8158a3d2f1ba2caa056cd990717cf139
Will Turner <willguyturner@gmail.com>
2025-02-15 17:13:01 +1300

    Set up project structure

---
+++ main.py
@@ -0,0 +1,22 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Footer, Header
+
+class TodoApp(App):
+    """A textual organistion app."""
+
+    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets for the app."""
+        yield Header()
+        yield Footer()
+
+    def action_toggle_dark(self) -> None:
+        """An action to toggle dark mode."""
+        self.theme = (
+            "textual-dark" if self.theme == "textual-light" else "textual-light"
+        )
+
+if __name__ == "__main__":
+    app = TodoApp()
+    app.run()
